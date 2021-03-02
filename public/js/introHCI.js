2552a10b97460cca7adbbc9c98cbd15b3db8a112
Vivian Wu <vrwu@ucsd.edu>
2021-03-01 16:59:21 -0800

    debug files

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,24 +2,24 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+  initializePage();
 })
 
 // Function that is called when the document is ready.
 function initializePage() {
-    $.get('/lab/today', onServerResponse);
+  $.get('/lab/today', onServerResponse);
 }
 
 // Function that is called when the server responds to the AJAX call
-function onServerResponse(data){
-    var labElementId = getLabElementId(data.id);
-    var labElement = $(labElementId);
-    labElement.css({'background': 'yellow'});
+function onServerResponse(data) {
+  var labElementId = getLabElementId(data.id);
+  var labElement = $(labElementId);
+  labElement.css({'background': 'yellow'});
 }
 
 
 // Function to format the CSS selector
-function getLabElementId(id){
-    var labElementId = '#lab_' + id;
-    return labElementId;
+function getLabElementId(id) {
+  var labElementId = '#lab-' + id;
+  return labElementId;
 }
